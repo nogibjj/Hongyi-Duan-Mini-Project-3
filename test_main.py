16b6367e719e6f0a7107b424174391f0a6b85c1e
Lulu-1121 <hd162@duke.edu>
2024-09-22 11:57:15 +0800

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -1,7 +1,30 @@
-from main import add
+from main import *
+import polars as pl
 
-def test_add():
-    assert add(1, 2) == 3
+df = None
+try:
+    df = pl.read_excel("Project-Management-Sample-Data.xlsx")
+except FileNotFoundError:
+    pass
+
+x = df['Days Required']
+print(x.describe())
+
+def test_df_exists():
+    assert df is not None, "DataFrame was not loaded, check if it exists."
+    
+def test_mean():
+    assert round(get_mean(x), 5) == round(x.mean(), 5)
+    
+def test_median():
+    assert round(get_median(x), 5) == round(x.median(), 5)
+    
+def test_std():
+    assert round(get_std(x), 5) == round(x.std(), 5)
 
 if __name__ == "__main__":
-    test_add()
+    test_df_exists()
+    test_mean()
+    test_median()
+    test_std()
+    get_plot(x)
