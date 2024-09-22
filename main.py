9b030395ad81120523791517de118e488203c2be
Lulu-1121 <hd162@duke.edu>
2024-09-22 12:03:59 +0800

    main.py

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
 import matplotlib.pyplot as plt
 import polars as pl
 
-def get_mean(x):
-    return x.mean()
+def get_mean(df, column_name):
+    # Calculate mean using Polars for a given column
+    return df.select(pl.col(column_name).mean()).item()
 
-def get_median(x):
-    return x.median()
+def get_median(df, column_name):
+    # Calculate median using Polars for a given column
+    return df.select(pl.col(column_name).median()).item()
 
-def get_std(x):
-    return x.std()
+def get_std(df, column_name):
+    # Calculate standard deviation using Polars for a given column
+    return df.select(pl.col(column_name).std()).item()
 
-def get_plot(x):
-    plt.plot(x.to_list())  # Convert Polars Series to list for plotting
-    plt.title(x.name)
+def get_plot(df, column_name):
+    # Convert the column to a list and plot it
+    x = df.select(pl.col(column_name)).to_series()
+    plt.plot(x.to_list())
+    plt.title(column_name)
     plt.xlabel('X')
     plt.ylabel('Y')
     plt.show()
+
