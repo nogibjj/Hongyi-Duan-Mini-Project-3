4f5c52abe797ac9593f508c75ab59d9658e23155
Lulu-1121 <hd162@duke.edu>
2024-09-22 11:56:38 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
-def add(x, y):
-    return x + y
-  
-result = add(1, 2)
+import matplotlib.pyplot as plt
+import polars as pl
 
-print(f"Sum: 1, 2, {result}")
+def get_mean(x):
+    return x.mean()
+
+def get_median(x):
+    return x.median()
+
+def get_std(x):
+    return x.std()
+
+def get_plot(x):
+    plt.plot(x.to_list())  # Convert Polars Series to list for plotting
+    plt.title(x.name)
+    plt.xlabel('X')
+    plt.ylabel('Y')
+    plt.show()
