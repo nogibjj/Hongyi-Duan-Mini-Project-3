e15c7f41ff4757702f796d559cc526139226d2e3
Lulu-1121 <hd162@duke.edu>
2024-09-23 09:47:18 +0800

    a

--- test_main.py
+++ test_main.py
@@ -7,24 +7,24 @@ try:
 except FileNotFoundError:
     pass
 
-x = df['Days Required']
-print(x.describe())
+column_name = 'Progress'
+print(df[column_name].describe())
 
 def test_df_exists():
     assert df is not None, "DataFrame was not loaded, check if it exists."
     
 def test_mean():
-    assert round(get_mean(x), 5) == round(x.mean(), 5)
+    assert round(get_mean(df, column_name), 5) == round(df[column_name].mean(), 5)
     
 def test_median():
-    assert round(get_median(x), 5) == round(x.median(), 5)
+    assert round(get_median(df, column_name), 5) == round(df[column_name].median(), 5)
     
 def test_std():
-    assert round(get_std(x), 5) == round(x.std(), 5)
+    assert round(get_std(df, column_name), 5) == round(df[column_name].std(), 5)
 
 if __name__ == "__main__":
     test_df_exists()
     test_mean()
     test_median()
     test_std()
-    get_plot(x)
+    get_plot(df, column_name)
